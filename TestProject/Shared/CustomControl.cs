285bac424b2007293e0c50e426479e27a4032f73
Faruk Turkusic <faruk.turkusic@onbe.com>
2022-03-24 10:25:05 +0100

    add structure of project

---
+++ TestProject/Shared/CustomControl.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptanceTest.Shared
+{
+    internal class CustomControl
+    {
+        public static void EnterText(IWebElement webElement, string value) => webElement.SendKeys(value);
+
+        public static void Click(IWebElement webElement) => webElement.Click();
+
+        public static void SelectByText(IWebElement webElement, string text)
+        {
+            SelectElement selectElement = new SelectElement(webElement);
+            selectElement.SelectByText(text);
+        }
+
+        public static void SelectByIndex(IWebElement webElement, int index)
+        {
+            SelectElement selectElement = new SelectElement(webElement);
+            selectElement.SelectByIndex(index);
+        }
+
+    }
+}
