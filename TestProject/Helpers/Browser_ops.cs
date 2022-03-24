e762847df0f01e1b4da6ed2b27ea7c7efebdb469
Faruk Turkusic <faruk.turkusic@onbe.com>
2022-03-24 10:00:25 +0100

    init commit

---
+++ TestProject/Helpers/Browser_ops.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptanceTest.Helpers
+{
+    internal class Browser_ops
+    {
+        IWebDriver webDriver;
+        public void Init_Browser()
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+        }
+        public string Title
+        {
+            get { return webDriver.Title; }
+        }
+        public void Goto(string url)
+        {
+            webDriver.Url = url;
+        }
+        public void Close()
+        {
+            webDriver.Quit();
+        }
+        public IWebDriver getDriver
+        {
+            get { return webDriver; }
+        }
+    }
+}
---
+++ TestProject/Helpers/DriverHelper.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptanceTest
+{
+    internal class DriverHelper
+    {
+        public static IWebDriver? Driver { get; set; }
+
+        //public void start_ChromeBrowser()
+        //{
+        //    // Local Selenium WebDriver
+        //    Driver = new ChromeDriver();
+        //    Driver.Manage().Window.Maximize();
+        //}
+
+        /* API - Get Window Title */
+        public String getCurrWindowTitle()
+        {
+            String windowTitle = Driver.Title;
+            return windowTitle;
+        }
+        /* API - Get Window Handle */
+        public String getMainWinHandle(IWebDriver driver)
+        {
+            return driver.CurrentWindowHandle;
+        }
+        public Boolean closeAllWindows(String currWindowHandle)
+        {
+            IList<string> totWindowHandles = new List<string>(Driver.WindowHandles);
+
+            foreach (String WindowHandle in totWindowHandles)
+            {
+                Console.WriteLine(WindowHandle);
+                if (!WindowHandle.Equals(currWindowHandle))
+                {
+                    Driver.SwitchTo().Window(WindowHandle);
+                    Driver.Close();
+                }
+            }
+
+            Driver.SwitchTo().Window(currWindowHandle);
+            if (Driver.WindowHandles.Count == 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
